dfa6eaf940ed2c1193e21108f29eec5dcd99087a
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2018-11-20 21:47:51 +0530

    Add files via upload
    Rough idea of how to add values to the board(properties etc)

---
+++ Board_values.py
@@ -0,0 +1,45 @@
+player1_name = input('Enter Name for player 1:')
+player2_name = input('Enter Name for player 2:')
+
+
+    # assign each variable a key value in a dictionary as properties
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 12
+l = 13
+m = 14
+n = 15
+o = 16
+p = 17
+q = 18
+r = 19
+s = 20
+t = 21
+u = 22
+v = 23
+x = 24
+y = 26
+z = 27
+za= 28
+
+
+board_values={1:'test',2:'test2'}
+
+print(board_values)
+# TODO Add property names to each value of dict and then under each if statement we can add a print
+# TODO also add a variable under the if to see which statement has been activated
+
+for k in board_values:
+    if k == a:
+        print('tru')
+    if k==b:
+        print('hh')
+
